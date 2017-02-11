3a8d5baabdcfa76d2ff230a40289e74b35a68931
Jan Konopka <janfrederik.konopka@gmail.com>
2017-02-11 20:46:24 +0100

    fix two simple bugs and a weird one (class attributes)

--- npr_pm_rip2.py
+++ npr_pm_rip2.py
@@ -1,3 +1,10 @@
+# assembles a podcast feed (rss/xml) containing all planet money episodes
+#   (their official feed only includes the most recent episodes)
+# by downloading the human-interfacing HTML (which does contain all episodes, surprisingly),
+#   parsing it into python datatypes (PlanetMoneyHTMLParser), and emitting an xml rss feed
+
+# TODO: cache websites?
+
 from html.parser import HTMLParser
 from html.entities import name2codepoint
 from html import escape
@@ -8,15 +15,17 @@ import urllib.request
 
 class PlanetMoneyHTMLParser(HTMLParser):
 
-    prev = None
-    next_attr = ''
-    # stack tags (sneaking in before content) wa want to ignore in handle_data
-    # eg   <want> <time="12"> irrelevant data! </time> data we want </want>
-    # so here we would ignore 'time'
-    tag_stack = []
+    def __init__(self):
+        self.prev = None
+        self.next_attr = ''
+        # stack tags (sneaking in before content) wa want to ignore in handle_data
+        # eg   <want> <time="12"> irrelevant data! </time> data we want </want>
+        # so here we would ignore 'time'
+        self.tag_stack = []
 
-    feed_entry = {}
-    feed_entries = []
+        self.feed_entry = {}
+        self.feed_entries = []
+        super().__init__()
 
     def handle_starttag(self, tag, attrs):
 
@@ -34,10 +43,10 @@ class PlanetMoneyHTMLParser(HTMLParser):
             self.feed_entry['guid'] = attrs[0][1]
 
         if tag == 'time':
-            # if ('class', 'audio-module-duration') in attrs:
-            #     self.next_attr = 'itunes:duration'
-            # else:
-            self.feed_entry['pubDate'] = attrs[0][1]
+            if ('class', 'audio-module-duration') in attrs:
+                self.next_attr = 'itunes:duration'
+            else:
+                self.feed_entry['pubDate'] = attrs[0][1]
 
         self.prev = (tag, attrs)
         # XXX check tag,attrs instead of doing [0][1]
@@ -62,20 +71,19 @@ class PlanetMoneyHTMLParser(HTMLParser):
         self.next_attr = ''
 
 
-
-
 URL_STEM = 'http://www.npr.org/sections/money/127413729/podcast/archive'
-
 USER_AGENT = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 HDR = {'User-Agent': USER_AGENT, 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
+PLANET_MONEY_EPOCH = 2008
 
-print('making ~100 requests to gather urls, please be patient...')
+yr_now = datetime.datetime.now().year
+print('making ' + str(12 * (yr_now - PLANET_MONEY_EPOCH-1)) + ' requests to gather urls, please be patient...')
 req_nr = 0
-
 all_feed_entries = []
 
-for year in range(datetime.datetime.now().year, 2007, -1):
+for year in range(yr_now, PLANET_MONEY_EPOCH-1, -1):
     for month in range(12, 0, -1):
+
         req_nr += 1
         print('Request number ' + str(req_nr), end='\r')
 
@@ -89,9 +97,8 @@ for year in range(datetime.datetime.now().year, 2007, -1):
             parser = PlanetMoneyHTMLParser()
             parser.feed(the_page)
             for e in parser.feed_entries:
-                if all(f['link'] != e['link'] for f in all_feed_entries):
+                if all(f['link'] != e['link'] for f in all_feed_entries):  # prevent dupes
                     all_feed_entries.append(e)
-
 
 with open('/home/jan/Dropbox/py/planetmoney-rss/npr_pm_test.xml', 'w') as f:
     f.write('''<?xml version="1.0" encoding="utf-8"?>
@@ -100,10 +107,9 @@ with open('/home/jan/Dropbox/py/planetmoney-rss/npr_pm_test.xml', 'w') as f:
         <title>Planet Money but it's all episodes</title>
         <link>https://github.com/xjcl/planetmoney-rss/tree/gh-pages</link>
         <image><url>http://nationalpublicmedia.com/wp-content/uploads/2014/06/planetmoney.png</url></image>
-        <description>pls don't sue</description>
-    ''')
+        <description>pls don't sue</description>\n''')
 
-    for e in parser.feed_entries:
+    for e in all_feed_entries:
         f.write('<item>')
         for k,v in e.items():
             f.write('<' + k + '>' + escape(v) + '</' + k + '>')
