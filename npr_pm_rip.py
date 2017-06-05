162e0c6fa1a28a99f8941ab26b6092dfe27f7a65
Jan Konopka <janfrederik.konopka@gmail.com>
2017-06-05 14:56:27 +0200

    unhelpfully separate into functions

--- npr_pm_rip.py
+++ npr_pm_rip.py
@@ -79,73 +79,90 @@ class PlanetMoneyHTMLParser(HTMLParser):
         self.next_attr = ''
 
 
-
+# ok not to subtract one here cos there was only a single episode that date
 PLANET_MONEY_EPOCH = datetime.datetime(2008, 9, 9)
 FEED_PICKLE_FILE = 'npr_pm_feed.p'
 URL_STEM = 'http://www.npr.org/sections/money/127413729/podcast/archive'
 
 
+
 # try to load cached results from a previous run of this script
-try:
-    with open(FEED_PICKLE_FILE, 'rb') as f:
-        # i think we have to store the rss feed "newest-first"
-        #     (everyone else does it, looks dum in firefox if "oldest-first")
-        all_feed_entries = pickle.load(f)
-    epoch = datetime.datetime.strptime(all_feed_entries[0]['pubDate'], '%Y-%m-%d') - datetime.timedelta(days=1)
-except:
-# if True:
-    all_feed_entries = []
-    epoch = PLANET_MONEY_EPOCH
+def load_feed_entries():
+    try:
+        with open(FEED_PICKLE_FILE, 'rb') as f:
+            # i think we have to store the rss feed "newest-first"
+            #     (everyone else does it, looks dum in firefox if "oldest-first")
+            old_feed_entries = pickle.load(f)
+        epoch = datetime.datetime.strptime(old_feed_entries[0]['pubDate'], '%Y-%m-%d') - datetime.timedelta(days=1)
+
+    except:
+        old_feed_entries = []
+        epoch = PLANET_MONEY_EPOCH
+
+    return (old_feed_entries, epoch)
 
 
-now = datetime.datetime.now()
-print('making ~' + str(math.ceil((now - epoch).days / 40)) + ' requests to gather urls, please be patient...')
-req_nr = 0
 
-new_feed_entries = []
+def parse_site_into_feed(old_feed_entries, epoch):
 
-# we have to iteratre from present to the past because we need to know the last date to make the next request
-curdate = now
-while curdate > epoch:
+    now = datetime.datetime.now()
+    print('making ~' + str(math.ceil((now - epoch).days / 40)) + ' requests to gather urls, please be patient...')
+    req_nr = 0
 
-    req_nr += 1
-    print('Request number', req_nr, 'for date', curdate.strftime('%Y-%m-%d'), end='\r')
+    new_feed_entries = []
 
-    full_url = URL_STEM + curdate.strftime('?date=%m-%d-%Y')  # site uses yankeedates !! lmao
-    req = urllib.request.Request(full_url)
+    # we have to iteratre from present to the past because we need to know the last date to make the next request
+    curdate = now
+    while curdate > epoch:
 
-    with urllib.request.urlopen(req) as response:
-        the_page = str(response.read(), 'utf-8')
+        req_nr += 1
+        print('Request number', req_nr, 'for date', curdate.strftime('%Y-%m-%d'), end='\r')
+
+        full_url = URL_STEM + curdate.strftime('?date=%m-%d-%Y')  # site uses yankeedates !! lmao
+        req = urllib.request.Request(full_url)
+
+        with urllib.request.urlopen(req) as response:
+            the_page = str(response.read(), 'utf-8')
 
         parser = PlanetMoneyHTMLParser()
         parser.feed(the_page)
+        parser.close()
+
         for e in parser.feed_entries:
             curdate = datetime.datetime.strptime(e['pubDate'], '%Y-%m-%d')
-            if all(f['link'] != e['link'] for f in all_feed_entries) and \
+            if all(f['link'] != e['link'] for f in old_feed_entries) and \
                all(f['link'] != e['link'] for f in new_feed_entries):  # prevent duplicates
                 new_feed_entries.append(e)
 
+    return new_feed_entries
 
 
-all_feed_entries = new_feed_entries + all_feed_entries
 
-with open('npr_pm_test.xml', 'w') as f:
-    f.write('''<?xml version="1.0" encoding="utf-8"?>
-        <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
-        <channel>
-        <title>Planet Money but it's all episodes</title>
-        <link>https://github.com/xjcl/planetmoney-rss/tree/gh-pages</link>
-        <image><url>http://nationalpublicmedia.com/wp-content/uploads/2014/06/planetmoney.png</url></image>
-        <description>pls don't sue</description>\n''')
+def save_feed_entries(all_feed_entries):
 
-    for e in all_feed_entries:
-        f.write('<item>')
-        for k,v in sorted(e.items()):
-            f.write('<' + k + '>' + escape(v) + '</' + k + '>')
-        f.write('</item>\n')
+    with open('npr_pm_test.xml', 'w') as f:
+            f.write('''<?xml version="1.0" encoding="utf-8"?>
+                <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
+                <channel>
+                <title>Planet Money but it's all episodes</title>
+                <link>https://github.com/xjcl/planetmoney-rss/tree/gh-pages</link>
+                <image><url>http://nationalpublicmedia.com/wp-content/uploads/2014/06/planetmoney.png</url></image>
+                <description>pls don't sue</description>\n''')
 
-    f.write('</channel></rss>\n')
+            for e in all_feed_entries:
+                f.write('<item>')
+                for k,v in sorted(e.items()):
+                    f.write('<' + k + '>' + escape(v) + '</' + k + '>')
+                f.write('</item>\n')
 
-with open(FEED_PICKLE_FILE, 'wb') as f:
-    pickle.dump(all_feed_entries, f)
+            f.write('</channel></rss>\n')
 
+    with open(FEED_PICKLE_FILE, 'wb') as f:
+        pickle.dump(all_feed_entries, f)
+
+
+
+if __name__ == '__main__':
+    old_feed_entries, epoch = load_feed_entries()
+    new_feed_entries = parse_site_into_feed(old_feed_entries, epoch)
+    save_feed_entries(new_feed_entries + old_feed_entries)
